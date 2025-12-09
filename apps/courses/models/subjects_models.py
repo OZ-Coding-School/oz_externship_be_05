50600ddfbe5679dbffdee0feb52824e1e11f71fa
dddops <Eopseum0@gmail.com>
2025-12-09 20:51:12 +0900

    🐛 fix: courses column fix 반려 사항 수정

--- apps/courses/models/subjects_models.py
+++ apps/courses/models/subjects_models.py
@@ -10,17 +10,14 @@ class SubjectChoices(models.TextChoices):
 
 
 class Subject(models.Model):
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
-    number_of_days = models.SmallIntegerField(max_length=2)
-    number_of_hours = models.SmallIntegerField(max_length=3)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, unique=True)
+    number_of_days = models.SmallIntegerField()
+    number_of_hours = models.SmallIntegerField()
     thumbnail_img_url = models.URLField(null=True, blank=True)
-    status = models.CharField(
-        choices=SubjectChoices.choices,
-        default=SubjectChoices.PENDING,
-    )
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "Subjects"
+        db_table = "subjects"
