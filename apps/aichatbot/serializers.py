c5fda1254e4aa06cfc8150c83fc02cb0ff6f45fc
hochang0819 <strongandrew@naver.com>
2025-12-09 20:19:42 +0900

    feat: models.py → models/ 수정

--- apps/aichatbot/serializers.py
+++ apps/aichatbot/serializers.py
@@ -1,15 +1,51 @@
 from rest_framework import serializers
 
-from .models import ChatbotCompletion, ChatbotSession
+from apps.aichatbot.models.chatbot_completions import ChatbotCompletion
+from apps.aichatbot.models.chatbot_sessions import ChatbotSession
+from apps.question.models import Question
 
-# # 세션 생성 요청(question_id 받기)
-# class SessionCreateSerializer(serializers.ModelSerializer):
-#
-# # 세션 목록 조회 응답
-# class SessionListSerializer(serializers.ModelSerializer):
-#
-# # 채팅 요청(prompt 받기)
-# class CompletionCreateSerializer(serializers.ModelSerializer):
-#
-# # 대화 내역 응답하기
-# class CompletionListSerializer(serializers.ModelSerializer):
+
+# 세션 생성 요청(question_id 받기)
+class SessionCreateSerializer(serializers.ModelSerializer[Question]):
+    # question_id = serializers.IntegerField(
+    #     write_only=True,
+    #     help_text="Question ID",
+    # )
+
+    class Meta:
+        model = ChatbotSession
+        fields = "__all__"
+        read_only_fields = fields
+
+
+# 세션 목록 조회 응답
+class SessionListSerializer(serializers.ModelSerializer[Question]):
+    # question_id = serializers.IntegerField(source="question_id", read_only=True)
+    # question_title = serializers.CharField(
+    #     source="question.title",
+    #     read_only=True,
+    #     help_text="Question's title",
+    # )
+
+    class Meta:
+        model = ChatbotSession
+        fields = "__all__"
+        read_only_fields = fields
+
+
+# 채팅 요청(prompt 받기)
+class CompletionCreateSerializer(serializers.ModelSerializer[ChatbotCompletion]):
+
+    class Meta:
+        model = ChatbotCompletion
+        fields = "__all__"
+        read_only_fields = fields
+
+
+# 대화 내역 응답하기
+class CompletionListSerializer(serializers.ModelSerializer[ChatbotCompletion]):
+
+    class Meta:
+        model = ChatbotCompletion
+        fields = "__all__"
+        read_only_fields = fields
