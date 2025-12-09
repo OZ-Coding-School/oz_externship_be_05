0697884ace47bcb5fa701154c9807c97c5d54e05
hochang0819 <strongandrew@naver.com>
2025-12-09 17:16:27 +0900

    ✨ feat: aichatbot models.py 생성

---
+++ apps/aichatbot/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import ChatbotCompletion, ChatbotSession
+
+# # 세션 생성 요청(question_id 받기)
+# class SessionCreateSerializer(serializers.ModelSerializer):
+#
+# # 세션 목록 조회 응답
+# class SessionListSerializer(serializers.ModelSerializer):
+#
+# # 채팅 요청(prompt 받기)
+# class CompletionCreateSerializer(serializers.ModelSerializer):
+#
+# # 대화 내역 응답하기
+# class CompletionListSerializer(serializers.ModelSerializer):
