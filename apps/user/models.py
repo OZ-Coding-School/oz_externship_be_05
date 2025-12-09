ca92ede2827d93cd0555d724c4b4c1f3223c8570
choiguen <daindj0@gmail.com>
2025-12-09 11:34:34 +0900

    feat:user model 수정

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("이메일은 필수입니다.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
 
 class GenderChoices(models.TextChoices):
     MALE = 'M', 'MALE'
@@ -17,15 +31,20 @@ class RoleChoices(models.TextChoices):
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=20, unique=True)
-    phone_num = models.CharField(max_length=11)
-    gender = models.CharField(choices=GenderChoices.choices, max_length=1)
+    nickname = models.CharField(max_length=10, unique=True)
+    phone_num = models.CharField(max_length=20)
+    gender = models.CharField(choices=GenderChoices.choices, max_length=6)
     birthday = models.DateField()
     profile_image_url = models.URLField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["nickname", "name"]
 
     class Meta:
         db_table = "users"
