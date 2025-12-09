04c2316bed11e0bb8df995a042a5eeac7c5ef1f4
hochang0819 <strongandrew@naver.com>
2025-12-09 20:19:42 +0900

    feat: models.py → models/ 수정

---
+++ apps/aichatbot/models/chatbot_completions.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# ENUM용 클래스 - 유저
+class UserRole(models.TextChoices):
+    USER = "user", "USER"
+    ASSISTANT = "assistant", "ASSISTANT"
+
+
+class ChatbotCompletion(models.Model):
+    session_id = models.ForeignKey("chatbot_sessions.ChatbotSessions", on_delete=models.CASCADE)
+    message = models.TextField()
+    role = models.CharField(choices=UserRole.choices, max_length=14)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "chatbot_completions"
--- apps/aichatbot/models/chatbot_sessions.py
+++ apps/aichatbot/models/chatbot_sessions.py
@@ -10,24 +10,10 @@ class ChatModel(models.TextChoices):
 class ChatbotSession(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     question = models.ForeignKey("questions.Question", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "chatbot_session"
-
-
-# null=False는 기본이래여..
-class ChatbotCompletion(models.Model):
-    session_id = models.ForeignKey("chatbot_sessions.ChatbotSessions", on_delete=models.CASCADE)
-    prompt = models.TextField()
-    output = models.TextField()
+    title = models.CharField(max_length=30)
     using_model = models.CharField(choices=ChatModel.choices, max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "chatbot_completions"
-        # verbose_name =  "무언가"
-        # verbose_name_plural = "무언가"
-        # 굳이 안 적어도 될듯?
+        db_table = "chatbot_session"
