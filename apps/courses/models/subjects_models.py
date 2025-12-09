ff9d1324afd964b1f972bdabdca7385ec713f980
dddops <Eopseum0@gmail.com>
2025-12-09 19:23:50 +0900

    🐛 fix: courses column fix

--- apps/courses/models/subjects_models.py
+++ apps/courses/models/subjects_models.py
@@ -4,8 +4,9 @@ from .courses_models import Course
 
 
 class SubjectChoices(models.TextChoices):
-    Activated = "active"
-    Deactivated = "deactivated"
+    PENDING = "PENDING", "대기중"
+    IN_PROGRESS = "IN_PROGRESS", "처리중"
+    COMPLETED = "COMPLETED", "처리완료"
 
 
 class Subject(models.Model):
@@ -15,8 +16,8 @@ class Subject(models.Model):
     number_of_hours = models.SmallIntegerField(max_length=3)
     thumbnail_img_url = models.URLField(null=True, blank=True)
     status = models.CharField(
-        max_length=11,
         choices=SubjectChoices.choices,
+        default=SubjectChoices.PENDING,
     )
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
