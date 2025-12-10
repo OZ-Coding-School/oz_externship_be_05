2776edf82557cd915706273438cdd7db9c3ad6ae
Jamily <ajin20823@gmail.com>
2025-12-10 14:03:10 +0900

    🎨 style: 코드 포매팅 수정

--- apps/exams/models/__init__.py
+++ apps/exams/models/__init__.py
@@ -1,4 +1,4 @@
 from .exam import Exam
-from .exam_question import ExamQuestion
 from .exam_deployment import ExamDeployment
+from .exam_question import ExamQuestion
 from .exam_submission import ExamSubmission
