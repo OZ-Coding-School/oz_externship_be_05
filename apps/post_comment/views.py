367a29bbe3beee4b4b42ab8bd90a910ffbd0ea89
sowon-bak <cruzean479@gmail.com>
2025-12-10 17:25:37 +0900

    first pr

--- apps/post_comment/views.py
+++ apps/post_comment/views.py
@@ -1,24 +1,15 @@
-import random
-from django.utils import timezone
-from drf_spectacular.utils import extend_schema
-from rest_framework import status
-from rest_framework.permissions import AllowAny
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from apps.users.models import User, GenderChoices, SocialUser, SocialProvider
-from apps.users.serializers import SignUpSerializer, UserInfoSerializer
 
 
 class PostCommentRetrieveAPIView(APIView):
 
     def get(self, request: Request, *args, **kwargs):
+
         pass
 
 class PostCommentCreateAPIView(APIView):
 
     def post(self, request: Request, *args, **kwargs):
+
         pass
 
 class PostCommentUpdateDeleteAPIView(APIView):
