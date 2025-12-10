2a8c62939b9e12296a1b975ea28aa7a103cdb641
sowon-bak <cruzean479@gmail.com>
2025-12-10 14:35:06 +0900

    views 작성 중

--- apps/post_comment/views.py
+++ apps/post_comment/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import random
+from django.utils import timezone
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.users.models import User, GenderChoices, SocialUser, SocialProvider
+from apps.users.serializers import SignUpSerializer, UserInfoSerializer
+
+
+class PostCommentAPIView(APIView):
+
+    def post(self, request: Request, *args, **kwargs):
+
+
+        pass
+
+    def get(self, request: Request, *args, **kwargs):
+        pass
+
+    def put(self, request: Request, *args, **kwargs):
+        pass
+
+    def delete(self, request: Request, *args, **kwargs):
+        pass
+
