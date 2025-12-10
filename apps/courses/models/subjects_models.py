df705cc2d8a48f9a23baee3bb0364c09fd465c3c
dddops <Eopseum0@gmail.com>
2025-12-10 11:45:06 +0900

    🐛 fix: courses model fix

--- apps/courses/models/subjects_models.py
+++ apps/courses/models/subjects_models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from .courses_models import Course
+from apps.core.models import TimestampModel
+from apps.courses.models.courses_models import Course
 
 
 class SubjectChoices(models.TextChoices):
@@ -9,15 +10,13 @@ class SubjectChoices(models.TextChoices):
     COMPLETED = "COMPLETED", "처리완료"
 
 
-class Subject(models.Model):
+class Subject(TimestampModel):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=30, unique=True)
     number_of_days = models.SmallIntegerField()
     number_of_hours = models.SmallIntegerField()
     thumbnail_img_url = models.URLField(null=True, blank=True)
     status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "subjects"
