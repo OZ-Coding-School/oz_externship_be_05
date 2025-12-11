9e3aa2cc8b090ea888d0de145852e201b47339da
Jamily <ajin20823@gmail.com>
2025-12-12 06:53:09 +0900

    ✨ feat(#62): create exams CRUD view,service,urls
    - exams단에서 사용할 admin용 CRUD에 대한 view,service,urls를 작성하였습니다.
    - __init__.py 를 작성하여 디렉토리를 패키지화합니다.
    - ** view단의 Error Response 세분화가 필요합니다. **
    
    관련 이슈: #62
    
    # Conflicts:
    #	config/urls.py

---
+++ apps/exams/services/admin/__init__.py
@@ -0,0 +1,9 @@
+from .admin_exam_service import ExamService
+
+# from .admin_deployments_service import
+# from .admin_question_service import
+# from .admin_submission_service import
+
+__all__ = [
+    "ExamService",
+]
