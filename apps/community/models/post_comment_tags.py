a101ef5bcb0c2ca9585637aa28e7484f45cb875b
sowon-bak <cruzean479@gmail.com>
2025-12-11 10:19:28 +0900

    :recycle: core/models/TimeStampedModel 상속

--- apps/community/models/post_comment_tags.py
+++ apps/community/models/post_comment_tags.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 from apps.community.models.post_comment import PostComment
-from apps.user.models import User
 from apps.core.models import TimeStampedModel
+from apps.user.models import User
 
 
 class PostCommentTag(TimeStampedModel):
