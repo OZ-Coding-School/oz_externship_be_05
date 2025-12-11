2f97a9145d36de021822473afc322d036ee92db4
hochang0819 <strongandrew@naver.com>
2025-12-11 11:12:26 +0900

    :bug: fix: serializer 수정 to ModelSerializer, urls Camelcase 수정

--- apps/aichatbot/urls.py
+++ apps/aichatbot/urls.py
@@ -5,6 +5,6 @@ from apps.aichatbot.views import SessionDetailView, SessionGenerator, SessionLis
 
 urlpatterns = [
     path("sessions", SessionGenerator.as_view(), name="spec_session_generator"),
-    path("sessions/List", SessionListView.as_view(), name="spec_ChatbotSession"),
+    path("sessions/list", SessionListView.as_view(), name="spec_ChatbotSession"),
     path("sessions/<int:session_id>", SessionDetailView.as_view(), name="spec_ChatbotSessionDetail"),
 ]
