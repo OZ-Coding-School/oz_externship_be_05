9918584f9bb8b6f21a9e21a794c9093edeb45b67
sowon-bak <cruzean479@gmail.com>
2025-12-11 10:36:50 +0900

    :bug: models 수정

--- apps/community/models/post_comment.py
+++ apps/community/models/post_comment.py
@@ -9,8 +9,6 @@ class PostComment(TimeStampedModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "post_comments"
--- apps/community/models/post_comment_tags.py
+++ apps/community/models/post_comment_tags.py
@@ -8,8 +8,6 @@ from apps.user.models import User
 class PostCommentTag(TimeStampedModel):
     tagged_user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(PostComment, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "post_comment_tags"
