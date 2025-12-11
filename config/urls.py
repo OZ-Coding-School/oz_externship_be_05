e2902f591772433b5ea77fcf2c3b743554c1441f
choiguen <daindj0@gmail.com>
2025-12-11 15:45:30 +0900

    ✨ feat: user urls 경로 추가.

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("api/v1/qna/", include("apps.questions.urls")),
-    path("api/v1/accounts/", include("apps.user.urls"))
+    path("api/v1/accounts/", include("apps.user.urls")),
 ]
 
 if settings.DEBUG:
