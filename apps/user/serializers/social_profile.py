053f0f452ab56144b9674ae5d8f6fa67daf2032f
choiguen <daindj0@gmail.com>
2025-12-11 15:07:03 +0900

    ✨ feat: 소셜로그인 시리얼라이저 생성.

---
+++ apps/user/serializers/social_profile.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+
+class NaverProfileSerializer(serializers.Serializer):
+    id = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    name = serializers.CharField(required=True)
+    birthyear = serializers.CharField(required=True)
+    birthday = serializers.CharField(required=True)
+
+
+
+class KakaoProfileSerializer(serializers.Serializer):
+    id = serializers.CharField(required=True)
+    kakao_account = serializers.DictField(required=True)
+
+    def validate(self, attrs):
+        kakao_account = attrs.get("kakao_account", {})
+
+        profile = kakao_account.get("profile") or {}
+        if "nickname" not in profile:
+            raise serializers.ValidationError(
+                {"nickname": ["이 필드는 필수 항목입니다."]}
+            )
+
+        # birthday는 지금은 필수 x
+        # if "birthday" not in kakao_account:
+        #     raise serializers.ValidationError(
+        #         {"birthday": ["이 필드는 필수 항목입니다."]}
+        #     )
+
+        return attrs
