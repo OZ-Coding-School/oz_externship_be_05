cde95f8ad2f2de92d3530d59b42e59a30cec37c4
hochang0819 <strongandrew@naver.com>
2025-12-11 11:04:40 +0900

    :bug: fix: models의 생성시간,업데이트 시간 core의 timestamp 상속

--- apps/aichatbot/models/chatbot_sessions.py
+++ apps/aichatbot/models/chatbot_sessions.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from apps.core.models import TimeStampedModel
 
 # enum용 클래스 - 사용모델
 class ChatModel(models.TextChoices):
@@ -11,9 +11,9 @@ class ChatbotSession(models.Model):
     user = models.ForeignKey("user.User", on_delete=models.CASCADE, db_index=True)
     question = models.ForeignKey("questions.Question", on_delete=models.CASCADE, db_index=True)
     title = models.CharField(max_length=30)
-    using_model = models.CharField(choices=ChatModel.choices, max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    using_model = models.TextChoices(ChatModel.choices)
+    created_at = TimeStampedModel().created_at
+    updated_at = TimeStampedModel().updated_at
 
     class Meta:
         db_table = "chatbot_session"
