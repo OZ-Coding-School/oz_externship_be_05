d8806c626ea388f07d6199d0437a9195e43ce08f
Jamily <ajin20823@gmail.com>
2025-12-12 06:40:00 +0900

    ✨ feat(#62): admin permission code
    - 관리자 API에서 사용할 공통 페이징 설정지정
    - permission(IsAdminUser) 권한 및 정렬 필드 지정
    - __init__.py 명시

---
+++ apps/exams/permissions/__init__.py
@@ -0,0 +1,9 @@
+from apps.exams.permissions.admin_permission import (
+    AdminModelViewSet,
+    CustomPageNumberPagination,
+)
+
+__all__ = [
+    "CustomPageNumberPagination",
+    "AdminModelViewSet",
+]
---
+++ apps/exams/permissions/admin_permission.py
@@ -0,0 +1,27 @@
+from rest_framework import pagination, viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
+
+
+class CustomPageNumberPagination(pagination.PageNumberPagination):
+    """
+    관리자 API에서 사용할 공통 페이징 설정
+    - 페이지 번호 기반 (page)
+    - 페이지 크기 파라미터 이름: size
+    - 기본 페이지 크기: 10
+    - 최대 페이지 크기: 100
+    """
+    page_size_query_param = "size"
+    page_size = 10
+    max_page_size = 100
+
+
+class AdminModelViewSet(viewsets.ModelViewSet):
+    """
+    모든 관리자 전용 CRUD ViewSet이 상속받을 기본 클래스
+    - IsAdminUser 권한을 적용합니다.
+    """
+
+    permission_classes = [IsAdminUser]  # AllowAny: 개발환경 테스트용
+    pagination_class = CustomPageNumberPagination
+
+    valid_sort_fields = ["title", "created_at", "updated_at"]  # abc, 최신
