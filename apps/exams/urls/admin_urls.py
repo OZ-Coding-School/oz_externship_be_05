67edad977812ae2088634ea30ec8e67eceb0a763
Jamily <ajin20823@gmail.com>
2025-12-12 06:53:09 +0900

    ✨ feat(#62): create exams CRUD view,service,urls
    - exams단에서 사용할 admin용 CRUD에 대한 view,service,urls를 작성하였습니다.
    - __init__.py 를 작성하여 디렉토리를 패키지화합니다.
    - ** view단의 Error Response 세분화가 필요합니다. **
    
    관련 이슈: #62
    
    # Conflicts:
    #	config/urls.py

--- apps/exams/urls/admin_urls.py
+++ apps/exams/urls/admin_urls.py
@@ -1,3 +1,11 @@
 from django.urls import URLPattern, URLResolver
+from rest_framework.routers import DefaultRouter
 
-urlpatterns: list[URLPattern | URLResolver] = []
+from apps.exams.views.admin import ExamAdminViewSet
+
+router = DefaultRouter()  # CRUD에 필요한 모든 URL 패턴을 자동으로 생성
+
+# /api/v1/admin/exams 경로에 ExamAdminViewSet을 연결합니다.
+router.register(r"", ExamAdminViewSet, basename="exam")
+
+urlpatterns: list[URLPattern | URLResolver] = router.urls
