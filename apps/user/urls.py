c5aefe8ba7ca0601170a984bee8b1b3ebecfcc88
choiguen <daindj0@gmail.com>
2025-12-11 16:35:30 +0900

    🐛 fix: urls 오타수정.

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from apps.user.views import social_login_views
 
 urlpatterns = [
-    path("kakao/", social_login_views.KakaoLoginStartAPIView.as_view(), name="kakao-login-start"),
-    path("kakao/callback/", social_login_views.KakaoCallbackAPIView.as_view(), name="kakao-callback"),
+    path("social-login/kakao/", social_login_views.KakaoLoginStartAPIView.as_view(), name="kakao-login-start"),
+    path("social-login/kakao/callback/", social_login_views.KakaoCallbackAPIView.as_view(), name="kakao-callback"),
 
-    path("naver/", social_login_views.NaverLoginStartAPIView.as_view(), name="naver-login-start"),
-    path("naver/callback/", social_login_views.NaverCallbackAPIView.as_view(), name="naver-callback"),
+    path("social-login/naver/", social_login_views.NaverLoginStartAPIView.as_view(), name="naver-login-start"),
+    path("social-login/naver/callback/", social_login_views.NaverCallbackAPIView.as_view(), name="naver-callback"),
 ]
