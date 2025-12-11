14467beac9cd40b1c95c1ea7b44fe4d82b2d22f6
Jamily <ajin20823@gmail.com>
2025-12-12 06:31:09 +0900

    ✨ feat: create admin exam table serializer
    - exam의 serializer 작성.
    - 패키지 초기화 파일(__init__.py) 작성하여 패키지 명시.

--- apps/exams/serializers/admin/admin_exam_serializer.py
+++ apps/exams/serializers/admin/admin_exam_serializer.py
@@ -1,28 +1,36 @@
 from rest_framework import serializers
 
-from apps.courses.models import Subject
 from apps.exams.models import Exam, ExamDeployment, ExamQuestion, ExamSubmission
 
 
 class ExamSerializer(serializers.ModelSerializer):
     """
-    POST, PUT, GET (단일 조회) 요청 시 사용되는 기본 시리얼라이저입니다.
+    POST, PUT, GET (x단일 조회x) 요청 시 사용되는 기본 시리얼라이저입니다.
     """
 
     exam_title = serializers.CharField(source="title", max_length=50)
     subject_id = serializers.IntegerField(required=True)
+    subject_title = serializers.SerializerMethodField()
 
     class Meta:
         model = Exam
-        fields = ["id", "subject_id", "exam_title", "thumbnail_img_url", "created_at", "updated_at"]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ["id", "subject_id", "subject_title", "exam_title", "thumbnail_img_url", "created_at", "updated_at"]
+        read_only_fields = ["id", "subject_title", "created_at", "updated_at"]
+
+    @staticmethod
+    def get_subject_title(obj: Exam) -> str:
+        """
+        :param obj: Exam
+        :return:(str) Subject의 title을 반환
+        """
+        return obj.subject.title
 
 
 class ExamListSerializer(serializers.ModelSerializer):
     """
     GET /api/v1/admin/exams 요청 시 사용되는 목록 조회 시리얼라이저입니다.
     subject_name: subject_id
-    question_count: 문제개수(ExamQuestion)
+    question_count: 문제갯수(ExamQuestion)
     submit_count: 응시(제출)된 건수(ExamSubmission)
     """
 
@@ -37,22 +45,26 @@ class ExamListSerializer(serializers.ModelSerializer):
             "id",
             "exam_title",
             "subject_id",
-            "subject_name",  # 계산 필드
-            "question_count",  # 계산 필드
-            "submit_count",  # 계산 필드
+            "subject_name",     # subject title
+            "question_count",   # get_question_count
+            "submit_count",     # get_submit_count
             "created_at",
             "updated_at",
         ]
 
-    def get_question_count(self, obj: Exam) -> int:
+    @staticmethod
+    def get_question_count(obj: Exam) -> int:
         """
-        해당 시험(Exam)에 연결된 문제(ExamQuestion)의 개수를 반환합니다.
+        :param obj: Exam
+        :return: (int) 해당 시험(Exam)에 연결된 문제(ExamQuestion)의 개수를 반환합니다.
         """
         return obj.questions.count()
 
-    def get_submit_count(self, obj: Exam) -> int:
+    @staticmethod
+    def get_submit_count(obj: Exam) -> int:
         """
-        시험 배포(ExamDeployment)를 거쳐 총 제출(ExamSubmission)된 건수를 반환합니다.
+        :param obj: Exam
+        :return: (int)시험 배포(ExamDeployment)를 거쳐 총 제출(ExamSubmission)된 건수를 반환합니다.
         """
         count = 0
         # obj.deployments 해당 시험에 배포된 모든 인스턴스를 가져옵니다.
