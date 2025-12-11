53edced308a642acbdf8565d8318d25dab08b804
choiguen <daindj0@gmail.com>
2025-12-11 15:49:16 +0900

    🐛 fix: urls name 변경.

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from apps.user.views import social_login_views
 
 urlpatterns = [
-    path("social-login/kakao/",social_login_views.KakaoLoginStartAPIView.as_view(),name="카카오 Oauth 토큰 발급 API"),
-    path("social-login/naver/",social_login_views.NaverLoginStartAPIView.as_view(),name="네이버 Oauth 토큰 발급 API"),
-    path("social-login/kakao/callback",social_login_views.KakaoCallbackAPIView.as_view(),name="카카오 Oauth CallBack API"),
-    path("social-login/naver/callback",social_login_views.NaverCallbackAPIView.as_view(),name="네이버 Oauth CallBack API"),
+    path("kakao/", social_login_views.KakaoLoginStartAPIView.as_view(), name="kakao-login-start"),
+    path("kakao/callback/", social_login_views.KakaoCallbackAPIView.as_view(), name="kakao-callback"),
+
+    path("naver/", social_login_views.NaverLoginStartAPIView.as_view(), name="naver-login-start"),
+    path("naver/callback/", social_login_views.NaverCallbackAPIView.as_view(), name="naver-callback"),
 ]
