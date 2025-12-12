764287232889b0d4c1ba8e1c66bb065323b45e80
sowon-bak <cruzean479@gmail.com>
2025-12-12 19:16:45 +0900

    :sparkles: feat: create views

--- apps/community/models/post_comment_tags.py
+++ apps/community/models/post_comment_tags.py
@@ -4,7 +4,8 @@ from apps.community.models.post_comment import PostComment
 from apps.core.models import TimeStampedModel
 from apps.user.models import User
 
-#apps/community/models/post_comment_tags.py
+
+# apps/community/models/post_comment_tags.py
 class PostCommentTag(TimeStampedModel):
     tagged_user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(PostComment, on_delete=models.CASCADE)
