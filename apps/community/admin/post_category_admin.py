0a43aec2cae33eb943173501b77ad53be88522c8
dddops <Eopseum0@gmail.com>
2025-12-12 14:12:24 +0900

    🐛 fix      : community admin page fix

--- apps/community/admin/post_category_admin.py
+++ apps/community/admin/post_category_admin.py
@@ -2,14 +2,15 @@ from typing import TYPE_CHECKING
 
 from django.contrib import admin
 
-
 from apps.community.models.post_category import PostCategory
 
 if TYPE_CHECKING:
     from django.contrib.admin import ModelAdmin as _ModelAdmin
+
     BaseAdmin = _ModelAdmin[PostCategory]
 else:
     from django.contrib.admin import ModelAdmin as BaseAdmin
+
 
 @admin.register(PostCategory)
 class PostCategoryAdmin(BaseAdmin):
