184577febf4d74f85602bce4d6291fd9f25e77e8
sowon-bak <cruzean479@gmail.com>
2025-12-12 10:14:41 +0900

    :bug: fix: serializer fields fix

--- apps/community/serializers/post_comment_tags.py
+++ apps/community/serializers/post_comment_tags.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
+
 from apps.community.models.post_comment_tags import PostCommentTag
+
 
 class PostCommentTagsSerializer(serializers.ModelSerializer[PostCommentTag]):
     class Meta:
         model = PostCommentTag
         fields = [
-            "tagged_user_id",
-            "comment_id",
+            "tagged_user",
+            "comment",
         ]
         read_only_fields = ["id", "created_at", "updated_at"]
