e4c6d8c54cae6a3988cca1518bee8a0397c66c1c
dddops <Eopseum0@gmail.com>
2025-12-12 11:21:18 +0900

    ✨ feat: community admin page 작성 (post, post category)
    # Conflicts:
    #	config/settings/base.py

--- apps/community/models/post_category.py
+++ apps/community/models/post_category.py
@@ -7,5 +7,10 @@ class PostCategory(TimeStampedModel):
     name = models.CharField(max_length=20)
     status = models.BooleanField(default=False)
 
+    def __str__(self) -> str:
+        return self.name
+
     class Meta:
+        verbose_name = "Post Category"
+        verbose_name_plural = "Post Category"
         db_table = "post_category"
