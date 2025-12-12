592a9d147c642a533139313d2dddda7fcc7c7e37
sowon-bak <cruzean479@gmail.com>
2025-12-12 13:55:04 +0900

    :sparkles: feat: fix serializers and create views

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -1,14 +1,13 @@
 from django.urls import URLPattern, URLResolver, path
 
-from apps.community.views.post_comment import PostCommentListCreateAPIView,PostCommentUpdateDestroyAPIView
+from apps.community.views.post_comment import (
+    PostCommentListCreateAPIView,
+    PostCommentUpdateDestroyAPIView,
+)
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("{post_id}/comments",
-         PostCommentListCreateAPIView.as_view(),
-         name="post_comment_list_create"
-         ),
-    path("{post_id}/comments/{comment_id}",
-         PostCommentUpdateDestroyAPIView.as_view(),
-         name="post_comment_update_destroy"
-         ),
+    path("<int:post_id>/comments", PostCommentListCreateAPIView.as_view(), name="post_comment_list_create"),
+    path(
+        "<int:post_id>/comments/<int:comment_id>", PostCommentUpdateDestroyAPIView.as_view(), name="post_comment_update_destroy"
+    ),
 ]
