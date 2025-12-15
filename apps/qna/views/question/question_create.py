ebcd9de46f160c9db58b7422a5d5d896c49f4bf1
kihoon <nike6736@naver.com>
2025-12-15 15:15:53 +0900

    💡 chore : PR 리뷰 반영(핸들러/예외처리/서비스/spec url/view)

--- apps/qna/views/question/question_create.py
+++ apps/qna/views/question/question_create.py
@@ -3,14 +3,14 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.qna.exceptions.question_exceptions import (
-    QuestionCreateValidationError,
-)
 from apps.qna.permissions.question.question_create_permission import (
     QuestionCreatePermission,
 )
 from apps.qna.serializers.question.question_create import QuestionCreateSerializer
-from apps.qna.services.question.question_create_service import create_question
+from apps.qna.services.question.question_create_service import (
+    create_question,
+    get_category_or_raise,
+)
 from apps.user.models import User
 
 
@@ -22,15 +22,13 @@ class QuestionCreateAPIView(APIView):
         assert isinstance(user, User)
 
         serializer = QuestionCreateSerializer(data=request.data)
-        if not serializer.is_valid():
-            raise QuestionCreateValidationError()
+        serializer.is_valid(raise_exception=True)
+        category = get_category_or_raise(serializer.validated_data["category"])
 
         question = create_question(
             author=user,
-            title=serializer.validated_data["title"],
-            content=serializer.validated_data["content"],
-            category_id=serializer.validated_data["category"],
-            image_urls=serializer.validated_data.get("image_urls", []),
+            category=category,
+            validated_data=serializer.validated_data,
         )
 
         return Response(
