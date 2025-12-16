645bc80548e41c04f63479d692b7a0d2d802a4f4
choiguen <daindj0@gmail.com>
2025-12-16 17:31:40 +0900

    💡 chore: CI 관련 수정.

--- apps/admin_accounts/serializers.py
+++ apps/admin_accounts/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
-from apps.user.models import User
 from apps.courses.models.cohorts_models import Cohort
 from apps.courses.models.courses_models import Course
+from apps.user.models import User
 from apps.user.models.enrollment import StudentEnrollmentRequest
 from apps.user.models.withdraw import Withdrawal
+
 
 # 어드민 페이지 회원 목록 조회 API 시리얼라이저
 class AdminAccountListSerializer(serializers.ModelSerializer):
@@ -21,16 +22,19 @@ class AdminAccountListSerializer(serializers.ModelSerializer):
             "created_at",
         )
 
+
 # 어드민 페이지 회원 정보 상세 조회 API 시리얼라이저
 class CourseMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "tag")
 
+
 class CohortMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cohort
         fields = ("id", "number", "status", "start_date", "end_date")
+
 
 class AdminAccountRetrieveSerializer(serializers.ModelSerializer):
     assigned_courses = serializers.SerializerMethodField()
@@ -59,11 +63,14 @@ class AdminAccountRetrieveSerializer(serializers.ModelSerializer):
             if not cohort:
                 continue
             course = cohort.course
-            result.append({
-                "course": CourseMiniSerializer(course).data,
-                "cohort": CohortMiniSerializer(cohort).data,
-            })
+            result.append(
+                {
+                    "course": CourseMiniSerializer(course).data,
+                    "cohort": CohortMiniSerializer(cohort).data,
+                }
+            )
         return result
+
 
 # 어드민 페이지 회원 정보 수정 API 시리얼라이저
 class AdminAccountUpdateSerializer(serializers.ModelSerializer):
@@ -79,6 +86,7 @@ class AdminAccountUpdateSerializer(serializers.ModelSerializer):
             "profile_img_url",
         )
 
+
 # 수정 응답 시리얼라이저
 class AdminAccountResponseSerialzier(serializers.ModelSerializer):
     class Meta:
@@ -93,13 +101,15 @@ class AdminAccountResponseSerialzier(serializers.ModelSerializer):
             "gender",
             "status",
             "profile_img_url",
-            "updated_at"
+            "updated_at",
         )
+
 
 # 어드민 페이지 권한 변경 API 시리얼라이저
 ROLE_USER = {"U", "AD"}
 ROLE_COHORT = {"TA", "ST"}
 ROLE_COURSES = {"OM", "LC"}
+
 
 class AdminAccountRoleUpdateSerializer(serializers.Serializer):
     role = serializers.ChoiceField(choices=["U", "AD", "TA", "ST", "OM", "LC"])
@@ -113,15 +123,17 @@ class AdminAccountRoleUpdateSerializer(serializers.Serializer):
     def validate(self, attrs):
         role = attrs["role"]
         cohort_id_present = "cohort_id" in attrs and attrs.get("cohort_id") is not None
-        courses_present =  "assigned_courses" in attrs and attrs.get("assigned_courses") is not None
+        courses_present = "assigned_courses" in attrs and attrs.get("assigned_courses") is not None
 
         # 유저,어드민은 role만 수정 가능
         if role in ROLE_USER:
             if cohort_id_present or courses_present:
-                raise serializers.ValidationError({
-                    "detail": "USER/ADMIN은 role만 변경 가능합니다.",
-                    "allowed_fields": ["role"],
-                })
+                raise serializers.ValidationError(
+                    {
+                        "detail": "USER/ADMIN은 role만 변경 가능합니다.",
+                        "allowed_fields": ["role"],
+                    }
+                )
             return attrs
 
         # 학생/조교는 cohort_id 필수, assigned_courses 수정 금지
@@ -129,15 +141,21 @@ class AdminAccountRoleUpdateSerializer(serializers.Serializer):
             if not cohort_id_present:
                 raise serializers.ValidationError({"cohort_id": ["학생/조교 권한으로 변경 시 필수 필드입니다."]})
             if courses_present:
-                raise serializers.ValidationError({"assigned_courses": ["학생/조교 권한으로 변경할 수 없는 필드입니다."]})
+                raise serializers.ValidationError(
+                    {"assigned_courses": ["학생/조교 권한으로 변경할 수 없는 필드입니다."]}
+                )
             return attrs
 
         # 러닝코치/운영매니저는 assigned_courses 필수, cohort_id 수정 금지
         if role in ROLE_COURSES:
             if not courses_present:
-                raise serializers.ValidationError({"assigned_courses": ["러닝코치/운영매니저 권한으로 변경 시 필수 필드입니다."]})
+                raise serializers.ValidationError(
+                    {"assigned_courses": ["러닝코치/운영매니저 권한으로 변경 시 필수 필드입니다."]}
+                )
             if cohort_id_present:
-                raise serializers.ValidationError({"cohort_id": ["러닝코치/운영매니저 권한으로 변경할 수 없는 필드입니다."]})
+                raise serializers.ValidationError(
+                    {"cohort_id": ["러닝코치/운영매니저 권한으로 변경할 수 없는 필드입니다."]}
+                )
             return attrs
 
         return attrs
@@ -149,10 +167,12 @@ class CourseMiniSerializer(serializers.ModelSerializer):
         model = Course
         fields = ("id", "name", "tag")
 
+
 class CohortMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cohort
         fields = ("id", "number")
+
 
 class UserMiniSerializer(serializers.ModelSerializer):
     class Meta:
@@ -165,6 +185,7 @@ class UserMiniSerializer(serializers.ModelSerializer):
             "gender",
         )
 
+
 class AdminAccountStudentSerializer(serializers.ModelSerializer):
     cohort = CohortMiniSerializer(read_only=True)
     course = serializers.SerializerMethodField()
@@ -172,15 +193,8 @@ class AdminAccountStudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = StudentEnrollmentRequest
-        fields = (
-            "id",
-            "user",
-            "cohort",
-            "course",
-            "status",
-            "created_at"
-        )
-    
+        fields = ("id", "user", "cohort", "course", "status", "created_at")
+
     def get_course(self, obj):
         cohort = getattr(obj, "cohort", None)
         course = getattr(obj, "course", None) if cohort else None
@@ -218,6 +232,7 @@ class AdminAccountStudentEnrollRejectRequestSerializer(serializers.Serializer):
         required=True,
     )
 
+
 class AdminAccountStudentEnrollrejectResponseSerializer(serializers.Serializer):
     detail = serializers.CharField(max_length=255)
     success = serializers.IntegerField()
@@ -235,6 +250,7 @@ class UserMiniSerializer(serializers.ModelSerializer):
             "role",
             "birthday",
         )
+
 
 class AdminAccountWithdrawalListSerializer(serializers.ModelSerializer):
     user = UserMiniSerializer(read_only=True)
@@ -256,25 +272,14 @@ class AdminAccountWithdrawalListSerializer(serializers.ModelSerializer):
 class UserMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            "id",
-            "email",
-            "name",
-            "gender",
-            "role",
-            "status",
-            "profile_img_url",
-            "created_at"
-        )
+        fields = ("id", "email", "name", "gender", "role", "status", "profile_img_url", "created_at")
+
 
 class CourseMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = (
-            "id",
-            "name",
-            "tag"
-        )
+        fields = ("id", "name", "tag")
+
 
 class CohortMiniSerializer(serializers.ModelSerializer):
     class Meta:
@@ -287,12 +292,12 @@ class CohortMiniSerializer(serializers.ModelSerializer):
             "end_date",
         )
 
+
 class AdminAccountWithdrawalRetrieveSerializer(serializers.ModelSerializer):
     user = UserMiniSerializer(read_only=True)
     reason_display = serializers.CharField(source="get_reason_display", read_only=True)
     assigned_courses = serializers.SerializerMethodField()
     withdrawn_at = serializers.DateTimeField(source="created_at", read_only=True)
-
 
     class Meta:
         model = Withdrawal
@@ -314,9 +319,10 @@ class AdminAccountWithdrawalRetrieveSerializer(serializers.ModelSerializer):
             if not cohort:
                 continue
             course = cohort.course
-            result.append({
-                "course": CourseMiniSerializer(course).data,
-                "cohort": CohortMiniSerializer(cohort).data,
-            })
+            result.append(
+                {
+                    "course": CourseMiniSerializer(course).data,
+                    "cohort": CohortMiniSerializer(cohort).data,
+                }
+            )
         return result
-
