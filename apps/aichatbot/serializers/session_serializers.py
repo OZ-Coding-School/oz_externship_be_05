42ad9a7e2409e8456c63fa03c56cc9d669d062e4
hochang0819 <strongandrew@naver.com>
2025-12-16 18:04:46 +0900

    :bulb: chore: serializer 수정

--- apps/aichatbot/serializers/session_serializers.py
+++ apps/aichatbot/serializers/session_serializers.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rest_framework import serializers
 
 from apps.aichatbot.models.chatbot_sessions import ChatbotSession, ChatModel
@@ -37,32 +39,6 @@ Success Response Schema
     }
   ]
 }"
-
-Error Response Schema
-- POST
-"400: {
-    ""error_detail"": str
-}
-401: {
-    ""error_detail"": str
-}
-403: {
-    ""error_detail"": str
-}
-404: {
-    ""error_detail"": str
-}"
-
-- GET
-"400: {
-    ""error_detail"": str
-}
-401: {
-    ""error_detail"": str
-}
-403: {
-    ""error_detail"": str
-}"
 """
 
 
@@ -74,7 +50,6 @@ class SessionSerializer(serializers.ModelSerializer[ChatbotSession]):
         read_only_fields = fields
 
 
-# 세션 생성 요청(question_id 받기)
 # 한 시리얼라이저로 요청이랑 응답 스키마를 만들 수 있다. write_only랑 read_only 속성을 잘 사용
 class SessionCreateSerializer(serializers.ModelSerializer[ChatbotSession]):
     class Meta:
@@ -82,9 +57,25 @@ class SessionCreateSerializer(serializers.ModelSerializer[ChatbotSession]):
         fields = ["id", "user", "question", "title", "using_model", "created_at", "updated_at"]
         extra_kwargs = {
             "id": {"read_only": True},
+            "question": {"required": True},
             "user": {"read_only": True},
-            "using_model": {"read_only": True},
-            "title": {"read_only": True},
             "created_at": {"read_only": True},
             "updated_at": {"read_only": True},
+            "title": {"required": True},
+            "using_model": {"required": True},
         }
+
+    def create(self, validated_data: dict[str, Any]) -> ChatbotSession:
+        request = self.context["request"]
+        user = request.user
+        question = validated_data["question"]
+        title = validated_data.get("title") or "New Chat"
+        using_model = validated_data["using_model"]
+
+        session: ChatbotSession = ChatbotSession.objects.create(
+            user=user,
+            question=question,
+            title=title,
+            using_model=using_model,
+        )
+        return session
