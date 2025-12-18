8754c0ebedc06f1cb16879152db1b1cadfb7d87a
김민주 <minjuju518@naver.com>
2025-12-18 10:53:31 +0900

    🐛 fix: url 경로에 맞게 수정

--- apps/exams/urls/student_urls.py
+++ apps/exams/urls/student_urls.py
@@ -5,7 +5,7 @@ from ..views.student.exam_submit_view import ExamSubmissionCreateAPIView
 urlpatterns: list[URLPattern | URLResolver] = [
     path(
         # student/submit
-        "/submissions/",
+        "submissions/",
         ExamSubmissionCreateAPIView.as_view(),
         name="exam_submit",
     )
