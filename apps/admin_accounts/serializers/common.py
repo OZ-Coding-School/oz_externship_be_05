6f940fbb4d0c7be29b469e938f85c8e317ce9c5d
choiguen <daindj0@gmail.com>
2025-12-18 11:36:30 +0900

    ✨ feat: 유저 어드민 시리얼라이저 생성.

--- apps/admin_accounts/serializers/common.py
+++ apps/admin_accounts/serializers/common.py
@@ -1,34 +1,53 @@
+from typing import Any
+
 from rest_framework import serializers
-from apps.user.models import User
+
 from apps.courses.models.cohorts_models import Cohort
 from apps.courses.models.courses_models import Course
+from apps.user.models import User, Withdrawal
 
-class CourseMiniSerializer(serializers.ModelSerializer):
+# status field 생성 class
+class StatusMixin(serializers.Serializer[dict[str, Any]]):
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, obj: User) -> str:
+        if Withdrawal.objects.filter(user=obj).exists():
+            return "withdrew"
+        return "active" if obj.is_active else "inactive"
+
+# course 응답 검증 시리얼라이저
+class CourseMiniSerializer(serializers.ModelSerializer[Course]):
     class Meta:
         model = Course
         fields = ("id", "name", "tag")
 
-class CohortMiniSerializer(serializers.ModelSerializer):
+
+# chort 응답 검증 시리얼라이저
+class CohortMiniSerializer(serializers.ModelSerializer[Cohort]):
     class Meta:
         model = Cohort
         fields = ("id", "number", "status", "start_date", "end_date")
 
-class CohortSimpleSerializer(serializers.ModelSerializer):
+
+class CohortSimpleSerializer(serializers.ModelSerializer[Cohort]):
     class Meta:
         model = Cohort
         fields = ("id", "number")
 
-class UserMiniSerializer(serializers.ModelSerializer):
+
+class UserMiniSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
         fields = ("id", "email", "name", "birthday", "gender")
 
-class UserWithdrawalMiniSerializer(serializers.ModelSerializer):
+
+class UserWithdrawalMiniSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
         fields = ("id", "email", "name", "role", "birthday")
 
-class UserWithdrawalDetailMiniSerializer(serializers.ModelSerializer):
+
+class UserWithdrawalDetailMiniSerializer(StatusMixin, serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ("id", "email", "name", "gender", "role", "status", "profile_img_url", "created_at")
+        fields = ("id", "email", "name", "gender", "role", "status", "profile_image_url", "created_at")
