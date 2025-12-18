3d1ed055a67990eefa179f1be12c2961cc4e718e
hochang0819 <strongandrew@naver.com>
2025-12-18 11:17:41 +0900

    :bulb: session 검증내역은 view에서 진행

--- apps/chatbot/serializers/completion_serializers.py
+++ apps/chatbot/serializers/completion_serializers.py
@@ -35,11 +35,7 @@ class CompletionCreateSerializer(serializers.ModelSerializer[ChatbotCompletion])
 
     def create(self, validated_data: dict[str, Any]) -> ChatbotCompletion:
         session = self.context["session"]
-        if session is None:
-            raise serializers.ValidationError("Session does not exist")  # 여기 붙는게 맞나? 따로 함수를 쓰는게?
-
         message = validated_data["message"]
-
         completion: ChatbotCompletion = ChatbotCompletion.objects.create(
             session=session,
             message=message,
