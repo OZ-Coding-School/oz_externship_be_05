f3dcd1ac7d67f80ac0a2e811a95325039cc81533
sowon-bak <cruzean479@gmail.com>
2025-12-18 14:46:21 +0900

    :sparkles: feat: fix serializers

--- apps/community/serializers/post_comment.py
+++ apps/community/serializers/post_comment.py
@@ -9,41 +9,51 @@ from apps.community.serializers.post_comment_tags import PostCommentTagsSerializ
 
 # apps/community/serializers/post_comment.py
 class PostCommentSerializer(serializers.ModelSerializer[PostComment]):
-    tags = PostCommentTagsSerializer(many=True, required=False)
+    tagged_user = PostCommentTagsSerializer(many=True, required=False)
 
     class Meta:
         model = PostComment
         fields = [
-            "tags",
+            "id",
+            "author",
+            "tagged_user",
             "content",
+            "created_at",
+            "updated_at",
         ]
         read_only_fields = ["id", "author", "post", "created_at", "updated_at"]
 
     def create(self, validated_data: Dict[str, Any]) -> PostComment:
-        tags_data = validated_data.pop("tags", [])
+        tags_data = validated_data.pop("tagged_user", [])
         comment = PostComment.objects.create(**validated_data)
 
-        # 기존 시리얼라이저로 태그 생성
+        # set() = 중복을 허용하지 않는 자료형
+        destroy_clone = set()
         for tag_data in tags_data:
-            PostCommentTag.objects.create(comment=comment, tagged_user=tag_data["tagged_user"])
+            destroy_clone.add(tag_data["tagged_user"].id)
+
+        # 중첩된 시리얼라이저로 태그 생성
+        for tag_id in destroy_clone:
+            PostCommentTag.objects.create(comment=comment, tagged_user_id=tag_id)
 
         return comment
 
     def update(self, comment: PostComment, validated_data: Dict[str, Any]) -> PostComment:
-        tags_data = validated_data.pop("tags", None)
+        tags_data = validated_data.pop("tagged_user", None)
 
         for content, data in validated_data.items():
             setattr(comment, content, data)
 
         comment.save()
 
+        PostCommentTag.objects.filter(comment=comment).delete()
+
         if tags_data is not None:
-            PostCommentTag.objects.filter(comment=comment).delete()
-
+            destroy_clone = set()
             for tag_data in tags_data:
-                PostCommentTag.objects.create(comment=comment, tagged_user=tag_data["tagged_user"])
+                destroy_clone.add(tag_data["tagged_user"].id)
 
-        else:
-            pass
+            for tag_id in destroy_clone:
+                PostCommentTag.objects.create(comment=comment, tagged_user_id=tag_id)
 
         return comment
