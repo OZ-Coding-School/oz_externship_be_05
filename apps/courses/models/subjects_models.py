5599c55862bf93b09e5958154f31aca3a4b1ac92
dddops <Eopseum0@gmail.com>
2025-12-18 18:02:52 +0900

    ✨ feat(#117): create subjects admin page

--- apps/courses/models/subjects_models.py
+++ apps/courses/models/subjects_models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from apps.core.models import TimeStampedModel
@@ -13,8 +14,12 @@ class SubjectChoices(models.TextChoices):
 class Subject(TimeStampedModel):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=30, unique=True)
-    number_of_days = models.SmallIntegerField()
-    number_of_hours = models.SmallIntegerField()
+    number_of_days = models.SmallIntegerField(
+        validators=[MinValueValidator(1)],
+    )
+    number_of_hours = models.SmallIntegerField(
+        validators=[MinValueValidator(1)],
+    )
     thumbnail_img_url = models.URLField(null=True, blank=True)
     status = models.BooleanField(default=True)
 
@@ -25,3 +30,7 @@ class Subject(TimeStampedModel):
         verbose_name = "Subject"
         verbose_name_plural = "Subjects"
         db_table = "subjects"
+        constraints = [
+            models.CheckConstraint(check=models.Q(number_of_days__gte=1), name="number_of_days_non_negative"),
+            models.CheckConstraint(check=models.Q(number_of_hours__gte=1), name="number_of_hours_non_negative"),
+        ]
