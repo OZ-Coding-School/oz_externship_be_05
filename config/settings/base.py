c2ad898eb90f59821da1e4ef906d9f5de1c9e234
choiguen <daindj0@gmail.com>
2025-12-18 15:25:08 +0900

    💡 chore(81): admin_accounts 앱 제거.

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,6 @@ CUSTOM_APPS: list[str] = [
     "apps.qna",
     "apps.community",
     "apps.chatbot",
-    "apps.admin_accounts",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
