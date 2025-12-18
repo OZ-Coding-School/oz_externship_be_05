2d8033c15b223c1d096a81475c839927fedfd296
ji-min0 <k.jimin2002@gmail.com>
2025-12-19 02:22:34 +0900

    :sparkles: feat(#93): 배포목록 조회 응답용 시리얼라이저 등록

--- apps/exams/serializers/admin/__init__.py
+++ apps/exams/serializers/admin/__init__.py
@@ -1,12 +1,15 @@
-from .admin_exam_serializer import ExamListSerializer, ExamSerializer
+from apps.exams.serializers.admin.admin_deployment_serializer import (
+    DeploymentListResponseSerializer,
+    ErrorResponseSerializer,
+)
+from apps.exams.serializers.admin.admin_exam_serializer import (
+    ExamListSerializer,
+    ExamSerializer,
+)
 
-# from .admin_deployment_serializer import
 # from .admin_exam_detail_serializer import
 # from .admin_question_serializer import
 # from .admin_submission_detail_serializer import
 # from .admin_submission_serializer import
 
-__all__ = [
-    "ExamSerializer",
-    "ExamListSerializer",
-]
+__all__ = ["ExamSerializer", "ExamListSerializer", "ErrorResponseSerializer", "DeploymentListResponseSerializer"]
