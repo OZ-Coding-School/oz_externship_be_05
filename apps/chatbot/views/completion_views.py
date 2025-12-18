69aa6567a4abdbd81ea2bc1730f9c6a55be21370
hochang0819 <strongandrew@naver.com>
2025-12-19 02:34:38 +0900

    :sparkles: feature(#110): completion POST 기능 구현

--- apps/chatbot/views/completion_views.py
+++ apps/chatbot/views/completion_views.py
@@ -1,19 +1,20 @@
-from typing import Optional
+from typing import Union
 
-from django.contrib.auth import user_logged_in
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.db.models import QuerySet
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
 from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.exceptions import ValidationError, NotFound, PermissionDenied
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-
-from apps.chatbot.models import ChatbotSession, ChatbotCompletion
-from apps.chatbot.serializers.completion_serializers import CompletionSerializer, CompletionCreateSerializer
+from apps.chatbot.models import ChatbotCompletion, ChatbotSession
+from apps.chatbot.serializers.completion_serializers import (
+    CompletionCreateSerializer,
+    CompletionSerializer,
+)
 
 
 class CompletionCreateAPIView(APIView):
@@ -21,51 +22,87 @@ class CompletionCreateAPIView(APIView):
 
     # session 존재여부 검증
     def _get_session_check(self, session_id: int) -> ChatbotSession:
-        if ChatbotSession.objects.filter(id=session_id).exists():
+        try:
             return ChatbotSession.objects.get(id=session_id)
-        else:
+        except ChatbotSession.DoesNotExist:
             raise NotFound("Session not found")
 
-    # 세션 종속여부 확인(are you my master?)
-    # 본인 아닐 시
-    def _validate_session_master(self, session: ChatbotSession, user: AbstractBaseUser) -> Optional[PermissionDenied]:
-        if not session.user == user:
-            raise PermissionDenied("You are not authorized")
+    # 세션 종속여부 확인
+    def _session_dependency(self, session: ChatbotSession, user: Union[AbstractBaseUser, AnonymousUser]) -> None:
+        if session.user != user:
+            raise NotFound("Session not found")
 
-    # SSE 응답 by chatbot
-    def _stream_assistant_response(self, session: ChatbotSession, user_message: str) -> Response:
-        # 로직 적기...뭐적어야하냐...
+    def _ai_chat_response(self, session: ChatbotSession, user_message: str) -> ChatbotCompletion:
+        # ai 챗봇 답변. services에서 구현
+        ai_response = f"Reply to '{user_message}'"
+
+        # AI 응답 DB에 저장
+        ai_completion = ChatbotCompletion.objects.create(
+            session=session,
+            message=ai_response,
+            role="assistant",
+        )
+
+        return ai_completion
 
     @extend_schema(
-        tags = ["Completion"],
-        summary = "챗봇 응답 생성 API: SSE 스트리밍",
-        request = CompletionCreateSerializer,
-        parameters = [
+        tags=["AI 챗봇"],
+        summary="AI 챗봇 응답 생성 API",
+        description="AI 챗봇과 사용자의 메세지를 생성/저장하는 API. user 메세지 저장 → AI 응답 생성·저장, 이후 둘 다 반환",
+        request=CompletionCreateSerializer,
+        parameters=[
             OpenApiParameter(
                 name="pk",
                 type=OpenApiTypes.INT,
-                location=OpenApiParameter.PATH,
+                location="path",
                 description="세션 고유 ID 값",
             ),
         ],
-        responses = {
-            "200" : {"type": "string", "description": "SSE 스트리밍 응답",},
-            "400" : {"type": "object", "example": {"error_detail": "Message field is essential"}},
-            "401" : {"type": "object", "example": {"error_detail": "Authentication credentials were not provided."}},
-            "403" : {"type": "object", "example": {"error_detail": "No permission for this action"}},
-            "404" : {"type": "object", "example": {"error_detail": "Session not found"}},
+        responses={
+            "201": {
+                "type": "string",
+                "example": {
+                    "user_completion": {
+                        "id": 1,
+                        "session": 1,
+                        "message": "python이 뭐야?",
+                        "role": "user",
+                        "created_at": "2025-01-01",
+                    },
+                    "ai_completion": {
+                        "id": 2,
+                        "session": 1,
+                        "message": "python 설명",
+                        "role": "assistant",
+                        "created_at": "2025-01-01",
+                    },
+                },
+                "description": "SSE 스트리밍 응답",
+            },
+            "400": {"type": "object", "example": {"error_detail": "Message field is essential"}},
+            "401": {"type": "object", "example": {"error_detail": "Authentication credentials were not provided."}},
+            "404": {"type": "object", "example": {"error_detail": "Session not found"}},  # 403 대신 사용
         },
     )
 
-    #
-    def post(self, request: Request, pk: int) -> Response:
+    # completion 대화 생성
+    def post(self, request: Request, session_id: int) -> Response:
         # 세션 검증
-        session = self._get_session_check(pk)
-        self._validate_session_master(session, request.user)
+        session = self._get_session_check(session_id)
+        self._session_dependency(session, request.user)
 
         # 요청 데이터 검증, 사용자 메세지 저장
         serializer = CompletionCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        completion = serializer.save(session=session)
+        user_completion: ChatbotCompletion = serializer.save(session=session, role="user")
 
-        return Response(serializer.data)
+        # ai 응답 생성, 저장
+        ai_completion = self._ai_chat_response(session, user_completion.message)
+
+        return Response(
+            {
+                "user_completion": CompletionSerializer(user_completion).data,
+                "ai_completion": CompletionSerializer(ai_completion).data,
+            },
+            status=status.HTTP_201_CREATED,
+        )
