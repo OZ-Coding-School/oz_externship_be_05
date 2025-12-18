b7afe95610d8c406f3dcea63f76b05265a96a062
Jamily <ajin20823@gmail.com>
2025-12-18 11:16:00 +0900

    ♻️ refactor: 특정 파일 develop 기준으로 복구 및 커밋 제외
    필요없는 코드 내용에 대한 develop 브랜치 상태로 원복
    
    - apps/exams/permissions/__init__.py
    - apps/exams/tests/admin/__init__.py

--- apps/exams/permissions/__init__.py
+++
@@ -1,9 +0,0 @@
-from apps.exams.permissions.admin_permission import (
-    AdminModelViewSet,
-    CustomPageNumberPagination,
-)
-
-__all__ = [
-    "CustomPageNumberPagination",
-    "AdminModelViewSet",
-]
