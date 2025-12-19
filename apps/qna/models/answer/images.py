89b6cea858570534850de3aff5d46b01c3275058
lukascruise <rmeo4692@gmail.com>
2025-12-19 17:53:02 +0900

    refactor(#108)이미지 검증로직 서비스 이관, 모델최적화

--- apps/qna/models/answer/images.py
+++ apps/qna/models/answer/images.py
@@ -1,7 +1,4 @@
-from django.conf import settings
-from django.core.validators import RegexValidator
 from django.db import models
-
 from apps.core.models import TimeStampedModel
 from apps.qna.models.answer.answers import Answer
 
@@ -16,14 +13,8 @@ class AnswerImage(TimeStampedModel):
     )
 
     image_url = models.CharField(
-        max_length=255,
-        help_text="이미지 파일 경로 URL",
-        db_index=True,
-        validators=[
-            RegexValidator(
-                regex=r"^answer_images/", message="이미지 경로는 반드시 'answer_images/'로 시작하는 S3 Key여야 합니다"
-            )
-        ],
+        max_length=500,
+        help_text="S3 object Key",
     )
 
     class Meta:
@@ -31,7 +22,7 @@ class AnswerImage(TimeStampedModel):
         db_table = "answers_images"
         verbose_name = "답변 이미지"
         verbose_name_plural = "답변 이미지 목록"
-        ordering = ["-created_at"]  # 최신 이미지가 먼저 오도록 정렬
+        ordering = ["-created_at"]  
 
     def __str__(self) -> str:
         return f"{self.pk}번 이미지 (on Answer {self.answer_id}) - {self.image_url}"  # 어떤 답변에 붙은이미지인지 식별
