ac758df6193396d360380911f122645c8ce1d743
김민주 <minjuju518@naver.com>
2025-12-22 16:45:05 +0900

    ✨ feat: submission 추가

--- apps/exams/urls/admin_urls.py
+++ apps/exams/urls/admin_urls.py
@@ -4,8 +4,11 @@ from apps.exams.views.admin import (
     ExamAdminListCreateAPIView,
     ExamAdminRetrieveUpdateDestroyAPIView,
 )
+from apps.exams.views.admin.admin_submission_view import AdminSubmissionListAPIView
 
 urlpatterns: list[URLPattern | URLResolver] = [
+    # submissions
+    path("submissions/", AdminSubmissionListAPIView.as_view(), name="exam-submission"),
     # /api/v1/admin/exams/ 경로에 ExamAdminViewSet을 연결합니다.
     path("", ExamAdminListCreateAPIView.as_view(), name="exam"),
     path("<str:pk>/", ExamAdminRetrieveUpdateDestroyAPIView.as_view(), name="exam-detail"),
