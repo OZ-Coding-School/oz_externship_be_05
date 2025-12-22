cb860de69bebaf32e89ad24320e159b8dd043fa5
choiguen <daindj0@gmail.com>
2025-12-22 17:54:24 +0900

    🐛 fix: user model에 status 값 생성에 따른 수정.

--- apps/user/serializers/admin/accounts.py
+++ apps/user/serializers/admin/accounts.py
@@ -3,13 +3,10 @@ from typing import Any
 from rest_framework import serializers
 
 from apps.user.models import User
-from apps.user.serializers.admin.common import (
-    StatusMixin,
-)
 
 
 # 계정 목록 조회 시리얼라이저
-class AdminAccountListSerializer(StatusMixin, serializers.ModelSerializer[User]):
+class AdminAccountListSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
         fields = ("id", "email", "nickname", "name", "birthday", "status", "role", "created_at")
