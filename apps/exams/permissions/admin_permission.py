f50b7e6f312fa3549c0265d8c12f6b8a5f43fe5c
김민주 <minjuju518@naver.com>
2025-12-22 16:44:44 +0900

    🐛 fix: 리베이스 오류 해결

--- apps/exams/permissions/admin_permission.py
+++ apps/exams/permissions/admin_permission.py
@@ -1,27 +1,21 @@
-from django.contrib.auth.models import AnonymousUser
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import AllowAny, BasePermission
+from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from apps.core.exceptions.exception_messages import EMS
 from apps.user.models.user import RoleChoices
 
 
 class IsStaffOrAdmin(BasePermission):
     """
     관리자 권한 확인 Permission
-
-    - 인증되지 않은 사용자 → 401
-    - 관리자 권한 없는 사용자 → 403
     """
+
+    message = EMS.E403_QUIZ_PERMISSION_DENIED("관리자")
 
     def has_permission(self, request: Request, view: APIView) -> bool:
         user = request.user
-
-        # 인증 여부 확인 (401)
-        if not user or not user.is_authenticated:
-            return False
 
         # 슈퍼유저는 항상 허용
         if user.is_superuser:
@@ -33,11 +27,13 @@ class IsStaffOrAdmin(BasePermission):
         return role in {RoleChoices.TA, RoleChoices.LC, RoleChoices.OM, RoleChoices.AD}
 
 
-class AdminUserPermission(APIView):
+class AdminUserPermissionView(APIView):
     """
     모든 관리자 전용 view단이 상속받을 기본 클래스
-    - IsAdminUser 권한을 적용합니다.
+    - JWT 인증 필수 401
+    - 인증 안 됨 401
+    - 관리자(role 기반) 권한 필수 403
     """
 
-    authentication_classes = [JWTAuthentication, SessionAuthentication]
-    permission_classes = [IsStaffOrAdmin]  # AllowAny: 개발환경 테스트용
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsStaffOrAdmin]
--- apps/exams/views/admin/__init__.py
+++ apps/exams/views/admin/__init__.py
@@ -2,10 +2,20 @@ from apps.exams.views.admin.admin_exam_view import (
     ExamAdminListCreateAPIView,
     ExamAdminRetrieveUpdateDestroyAPIView,
 )
-from apps.exams.views.admin.admin_submission_view import AdminSubmissionListAPIView
 
-# from .admin_deployment_view import
+from .admin_deployment_view import (
+    AdminDeploymentDetailUpdateDeleteView,
+    DeploymentListCreateAPIView,
+    ExamDeploymentStatusAPIView,
+)
+
 # from .admin_question_view import
 # from .admin_submission_view import
 
-__all__ = ["ExamAdminListCreateAPIView", "ExamAdminRetrieveUpdateDestroyAPIView", "AdminSubmissionListAPIView"]
+__all__ = [
+    "ExamAdminListCreateAPIView",
+    "ExamAdminRetrieveUpdateDestroyAPIView",
+    "DeploymentListCreateAPIView",
+    "AdminDeploymentDetailUpdateDeleteView",
+    "ExamDeploymentStatusAPIView",
+]
