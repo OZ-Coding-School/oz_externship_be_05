c5783d1830a3958f9bdeb29758cab7f15423a9ce
김민주 <minjuju518@naver.com>
2025-12-22 16:44:24 +0900

    ✨ feat: submission 추가

--- apps/exams/views/admin/__init__.py
+++ apps/exams/views/admin/__init__.py
@@ -2,9 +2,10 @@ from apps.exams.views.admin.admin_exam_view import (
     ExamAdminListCreateAPIView,
     ExamAdminRetrieveUpdateDestroyAPIView,
 )
+from apps.exams.views.admin.admin_submission_view import AdminSubmissionListAPIView
 
 # from .admin_deployment_view import
 # from .admin_question_view import
 # from .admin_submission_view import
 
-__all__ = ["ExamAdminListCreateAPIView", "ExamAdminRetrieveUpdateDestroyAPIView"]
+__all__ = ["ExamAdminListCreateAPIView", "ExamAdminRetrieveUpdateDestroyAPIView", "AdminSubmissionListAPIView"]
