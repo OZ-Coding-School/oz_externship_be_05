4c43b1ebb013ded03c5b8a00f22b04cf86b88e6e
dddops <Eopseum0@gmail.com>
2025-12-22 16:58:02 +0900

    🐛 fix(#144): fix course api

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,5 +3,5 @@ from django.urls import URLPattern, URLResolver, path
 from apps.courses.views import CourseCohortsView
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("<int:course_id>/cohorts", CourseCohortsView.as_view(), name="course-cohort-list"),
+    path("cohorts", CourseCohortsView.as_view(), name="course-cohort-list"),
 ]
