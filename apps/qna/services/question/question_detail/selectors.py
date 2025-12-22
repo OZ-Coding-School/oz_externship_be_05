2833657651dd4144fe02f9295d62640efb96582d
kihoon <nike6736@naver.com>
2025-12-22 12:52:16 +0900

    ✨ feat(#114) : 답변 정렬 기능 추가

--- apps/qna/services/question/question_detail/selectors.py
+++ apps/qna/services/question/question_detail/selectors.py
@@ -3,13 +3,19 @@ from django.db.models import Prefetch
 from apps.qna.models import Answer, Question
 
 
-# Answer / Comment 모델에 정렬코드가 있어서 따로 추가 X
 def get_question_detail_queryset(question_id: int) -> Question | None:
     return (
         Question.objects.select_related("author", "category")
         .prefetch_related(
             "images",
-            Prefetch("answers", queryset=Answer.objects.select_related("author").prefetch_related("comments__author")),
+            Prefetch(
+                "answers",
+                queryset=(
+                    Answer.objects.select_related("author")
+                    .prefetch_related("comments__author")
+                    .order_by("-created_at")  # Answer 정렬 추가
+                ),
+            ),
         )
         .filter(id=question_id)
         .first()
