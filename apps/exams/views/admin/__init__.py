614db9265dbdeecbb1ed2fe8e5766d6d9714ce44
ji-min0 <k.jimin2002@gmail.com>
2025-12-23 15:58:45 +0900

    :sparkles: feat(#151): 상세 조회 view 추가

--- apps/exams/views/admin/__init__.py
+++ apps/exams/views/admin/__init__.py
@@ -3,9 +3,17 @@ from apps.exams.views.admin.admin_exam_view import (
     ExamAdminRetrieveUpdateDestroyAPIView,
 )
 
-from .admin_deployment_view import DeploymentListCreateAPIView
+from .admin_deployment_view import (
+    AdminDeploymentDetailUpdateDeleteView,
+    DeploymentListCreateAPIView,
+)
 
 # from .admin_question_view import
 # from .admin_submission_view import
 
-__all__ = ["ExamAdminListCreateAPIView", "ExamAdminRetrieveUpdateDestroyAPIView", "DeploymentListCreateAPIView"]
+__all__ = [
+    "ExamAdminListCreateAPIView",
+    "ExamAdminRetrieveUpdateDestroyAPIView",
+    "DeploymentListCreateAPIView",
+    "AdminDeploymentDetailUpdateDeleteView",
+]
