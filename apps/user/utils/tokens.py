a286e74b4a46a1aae6e00fbed7fc03f4d1ae18c6
4vpr <4shout4@gmail.com>
2025-12-23 17:59:43 +0900

    ✨ feat: 코드리뷰반영

--- apps/user/utils/tokens.py
+++ apps/user/utils/tokens.py
@@ -1,8 +1,23 @@
+from typing import Literal, cast
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from apps.user.models import User
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework import status
 
 
-def issue_token_pair(user: User) -> dict[str, str]:
-    refresh = RefreshToken.for_user(user)
-    return {"refresh_token": str(refresh), "access_token": str(refresh.access_token)}
+def issue_token_pair(refresh: RefreshToken) -> Response:
+    secure = getattr(settings, "SESSION_COOKIE_SECURE", False)
+    samesite = cast(
+        Literal["Lax", "Strict", "None", False] | None, getattr(settings, "SESSION_COOKIE_SAMESITE", "Lax")
+    )
+    response = Response({"access_token" : refresh.access_token},status=status.HTTP_200_OK)
+    response.set_cookie(
+        "refresh_token",
+        refresh,
+        httponly=True,
+        secure=secure,
+        samesite=samesite,
+        path="/",
+    )
+    return response
