d97b1a0e22d7cd79200936c78cc9ddd138076ddf
choiguen <daindj0@gmail.com>
2025-12-29 01:31:12 +0900

    ✨ feat: 수강생 관리 경로 추가.

--- apps/user/admin_urls.py
+++ apps/user/admin_urls.py
@@ -5,9 +5,23 @@ from apps.user.views.admin.accounts import (
     AdminAccountRetrieveUpdateView,
     AdminAccountRoleUpdateView,
 )
+from apps.user.views.admin.enrollments import (
+    AdminStudentEnrollAcceptView,
+    AdminStudentEnrollRejectView,
+    AdminStudentsEnrollViews,
+    AdminStudentView,
+)
 
 urlpatterns = [
     path("accounts", AdminAccountListAPIView.as_view(), name="admin-account-list"),
     path("accounts/<int:account_id>", AdminAccountRetrieveUpdateView.as_view(), name="admin-account-detail"),
     path("accounts/<int:account_id>/role", AdminAccountRoleUpdateView.as_view(), name="admin-account-update-role"),
+    path("students", AdminStudentView.as_view(), name="admin-student-list"),
+    path("students-enrollments", AdminStudentsEnrollViews.as_view(), name="admin-student-enrollments-list"),
+    path(
+        "students-enrollments/accept", AdminStudentEnrollAcceptView.as_view(), name="admin-student-enrollments-accept"
+    ),
+    path(
+        "students-enrollments/reject", AdminStudentEnrollRejectView.as_view(), name="admin-student-enrollments-reject"
+    ),
 ]
