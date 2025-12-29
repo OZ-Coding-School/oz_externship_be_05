2e30870137db6711f1226c2c9ab762fd47753901
sowon-bak <cruzean479@gmail.com>
2025-12-29 14:35:18 +0900

    :sparkles: feat: fix serializer,view,test

--- apps/community/tests/post_comment.py
+++ apps/community/tests/post_comment.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from typing import Any, Dict
 
-from django.db.models import QuerySet
-from django.test import TestCase
 from rest_framework.test import APITestCase
 
 from apps.community.models.post import Post
@@ -14,12 +12,12 @@ from apps.user.models import User
 
 class APITestCaseBase(APITestCase):
 
-    def check_response(self, response_data: Dict[str, Any], author_name: str, content: str) -> None:
+    def check_response(self, response_data: Dict[str, Any], author_id: int, content: str) -> None:
         self.assertIn("id", response_data)
         self.assertIn("content", response_data)
         self.assertIn("author", response_data)
 
-        self.assertEqual(User.objects.get(id=response_data["author"]).name, author_name)
+        self.assertEqual(response_data["author"]["id"], author_id)
         self.assertEqual(response_data["content"], content)
 
 
@@ -32,19 +30,24 @@ class TestPostCommentListCreateAPIView(APITestCaseBase):
         )
 
         self.other_user = User.objects.create_user(
-            name="other_user", password="password", email="other@other.com", birthday=datetime.now()
+            name="other_user", password="password", nickname="누구", email="other@other.com", birthday=datetime.now()
         )
 
         self.tagged_user = User.objects.create_user(
-            name="tagged_user", password="password", email="tagged@tagged.com", birthday=datetime.now()
+            name="tagged_user", password="password", nickname="태그", email="tagged@tagged.com", birthday=datetime.now()
         )
 
         category = PostCategory.objects.create(name="테스트")
 
         self.post = Post.objects.create(title="제목", content="내용", author=self.test_user, category=category)
 
-        PostComment.objects.create(post=self.post, author=self.test_user, content="댓글1")
-        PostComment.objects.create(post=self.post, author=self.test_user, content="댓글2")
+        self.comment1 = PostComment.objects.create(post=self.post, author=self.test_user, content="댓글1")
+        self.comment2 = PostComment.objects.create(post=self.post, author=self.test_user, content="댓글2")
+        self.comment3 = PostComment.objects.create(post=self.post, author=self.other_user, content="댓글3")
+
+        PostCommentTag.objects.create(comment=self.comment1, tagged_user=self.tagged_user)
+        PostCommentTag.objects.create(comment=self.comment1, tagged_user=self.other_user)
+        PostCommentTag.objects.create(comment=self.comment2, tagged_user=self.tagged_user)
 
         self.post_url = f"/api/v1/posts/{self.post.id}/comments"
 
@@ -55,13 +58,17 @@ class TestPostCommentListCreateAPIView(APITestCaseBase):
         response = self.client.get(self.post_url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data['count'],3)
 
-        check_content = ["댓글1", "댓글2"]
+        check_author_id = [1,1,2]
+        check_content = ["댓글1", "댓글2","댓글3"]
 
-        for i, comment in enumerate(response.data):
+        print(response.data)
+        print(response.data['results'])
 
-            self.check_response(comment, "test_user", check_content[i])
+        for i, comment in enumerate(response.data['results']):
+
+            self.check_response(comment, check_author_id[i], check_content[i])
 
     def test_get_comments_not_exist(self) -> None:
 
@@ -70,20 +77,42 @@ class TestPostCommentListCreateAPIView(APITestCaseBase):
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.data["error_detail"], "해당 게시글을 찾을 수 없습니다.")
 
-    def test_create_comment(self) -> None:
+    def test_create_comment_without_tag(self) -> None:
 
-        data = {"content": "새 댓글"}
+        data = {"content": "태그 없는 댓글"}
+        response = self.client.post(self.post_url, data)
+
+        print(response.data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["detail"], "댓글이 등록되었습니다.")
+
+    def test_create_comment_with_tag(self) -> None:
+
+        data = {"content": "@태그 @누구 태그 있는 댓글"}
         response = self.client.post(self.post_url, data)
 
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(PostComment.objects.filter(post=self.post).count(), 3)
+        self.assertEqual(response.data["detail"], "댓글이 등록되었습니다.")
 
-        self.check_response(response.data, "test_user", "새 댓글")
+        comment = PostComment.objects.get(content="@태그 @누구 태그 있는 댓글")
+        self.assertIsNotNone(comment)
+        self.assertEqual(comment.author, self.test_user)
+
+        tags = PostCommentTag.objects.filter(comment=comment)
+        check_author_id = [self.other_user.id,self.tagged_user.id]
+        tui = []
+        for tag in tags:
+            tui.append(tag.tagged_user.id)
+
+        self.assertEqual(check_author_id,tui)
 
     def test_create_comment_with_empty_content(self) -> None:
 
         data = {"content": ""}
         response = self.client.post(self.post_url, data)
+
+        print(response.data)
 
         self.assertEqual(response.status_code, 400)
 
