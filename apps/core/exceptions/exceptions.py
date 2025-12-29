6981b6093d396d98bb209d9a4a554e4132391aba
ji-min0 <k.jimin2002@gmail.com>
2025-12-29 19:45:53 +0900

    :recycle: refactor(#167): LockedException에서 __init__ 오버라이딩 제거

--- apps/core/exceptions/exceptions.py
+++ apps/core/exceptions/exceptions.py
@@ -1,7 +1,5 @@
-from typing import Optional
-
 from rest_framework import status
-from rest_framework.exceptions import APIException, ErrorDetail
+from rest_framework.exceptions import APIException
 
 
 class LockedException(APIException):
@@ -13,10 +11,3 @@ class LockedException(APIException):
     status_code = status.HTTP_423_LOCKED
     default_detail = "리소스가 잠겨있습니다."
     default_code = "locked"
-
-    def __init__(self, error_detail: Optional[str] = None):
-        detail = ErrorDetail(
-            error_detail if error_detail is not None else self.default_detail,
-            code=self.default_code,
-        )
-        super().__init__(detail=detail)
