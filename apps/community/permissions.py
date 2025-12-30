a7baefb650ab577f521fb46cc2762cb0c29ed081
minseok <minseokgyang@gmail.com>
2025-12-30 14:00:07 +0900

    :bug: fix(#105): permissions.py - 인증된 사용자만 접근 가능하게 설정

--- apps/community/permissions.py
+++ apps/community/permissions.py
@@ -1,7 +1,6 @@
-from typing import Any
-
 from rest_framework import permissions
 from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from apps.community.models.post import Post
 
@@ -11,7 +10,15 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     게시글 작성자만 수정/삭제 가능, 그 외에는 읽기만 가능
     """
 
-    def has_object_permission(self, request: Request, view: Any, obj: Post) -> bool:
+    SAFE_METHOD = "GET"
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        if request.method == self.SAFE_METHOD:
+            return True
+
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request: Request, view: APIView, obj: Post) -> bool:
         if request.method in permissions.SAFE_METHODS:
             return True
 
