9a38e527a714cd864ddfbf6516dcee390d78b45c
ji-min0 <k.jimin2002@gmail.com>
2025-12-30 10:28:45 +0900

    :art: style(#169): 코드 포매팅 수정

--- apps/exams/urls/student_urls.py
+++ apps/exams/urls/student_urls.py
@@ -2,7 +2,6 @@ from django.urls import URLPattern, URLResolver, path
 
 from apps.exams.views.student import (
     ExamAccessCodeVerifyView,
-    ExamQuestionView,
     ExamDeploymentStatusCheckView,
     ExamQuestionView,
     ExamResultView,
