834c65a9f5e5fa056b140d9f88223f3e56b59d96
minseok <minseokgyang@gmail.com>
2025-12-31 10:22:59 +0900

    :sparkles: feat(#240) 카테고리 목록조회 및 get parameters 작성

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from apps.community.views.post_views import (
     PostDetailAPIView,
     PostListCreateAPIView,
+    PostCategoryListAPIView,
 )
 
 urlpatterns = [
     path("", PostListCreateAPIView.as_view(), name="post-list-create"),
     path("<int:post_id>/", PostDetailAPIView.as_view(), name="post-detail"),
+    path("post-categories/", PostCategoryListAPIView.as_view(), name="post-category-list"),
 ]
