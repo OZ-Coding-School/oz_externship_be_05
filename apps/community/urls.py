b7881733a0a817acd3e861da36ae9ca425d61f2a
minseok <minseokgyang@gmail.com>
2025-12-31 15:08:56 +0900

    :bug: fix(#252) : 1차 피드백반영

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -11,12 +11,12 @@ from apps.community.views.post_views import (
 )
 
 urlpatterns = [
-    path("", PostListCreateAPIView.as_view(), name="post-list-create"),
-    path("<int:post_id>/", PostDetailAPIView.as_view(), name="post-detail"),
-    path("post-categories/", PostCategoryListAPIView.as_view(), name="post-category-list"),
-    path("<int:post_id>/comments", PostCommentListCreateAPIView.as_view(), name="post_comment_list_create"),
+    path("posts", PostListCreateAPIView.as_view(), name="post-list-create"),
+    path("posts/<int:post_id>", PostDetailAPIView.as_view(), name="post-detail"),
+    path("posts/categories", PostCategoryListAPIView.as_view(), name="post-category-list"),
+    path("posts/<int:post_id>/comments", PostCommentListCreateAPIView.as_view(), name="post_comment_list_create"),
     path(
-        "<int:post_id>/comments/<int:comment_id>",
+        "posts/<int:post_id>/comments/<int:comment_id>",
         PostCommentUpdateDestroyAPIView.as_view(),
         name="post_comment_update_destroy",
     ),
