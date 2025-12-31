4f5bd2d9e8eb15047f856f1dc92847f241fe4e89
sowon-bak <cruzean479@gmail.com>
2025-12-31 11:37:27 +0900

    :recycle: feat: fix postcomment view

--- apps/community/models/post_comment_tags.py
+++ apps/community/models/post_comment_tags.py
@@ -4,6 +4,7 @@ from apps.community.models.post_comment import PostComment
 from apps.core.models import TimeStampedModel
 from apps.user.models import User
 
+
 class PostCommentTag(TimeStampedModel):
     tagged_user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(PostComment, on_delete=models.CASCADE)
