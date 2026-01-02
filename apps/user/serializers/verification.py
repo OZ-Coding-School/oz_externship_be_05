144b83e7cf122722e2ea49b45ed1cc0bcd7dd2ea
4vpr <4shout4@gmail.com>
2026-01-02 20:47:16 +0900

    🎨 style: black, isort 코드 포매터 실행

--- apps/user/serializers/verification.py
+++ apps/user/serializers/verification.py
@@ -14,6 +14,7 @@ class RequestPurpose(TextChoices):
     FIND = "find"
     RESTORE = "restore"
 
+
 class EmailRequestSerializer(Serializer[Any], BaseMixin):
     purpose = serializers.ChoiceField(choices=RequestPurpose.choices, default=RequestPurpose.SIGNUP)
     email = BaseMixin.get_email_field()
@@ -25,11 +26,11 @@ class EmailRequestSerializer(Serializer[Any], BaseMixin):
         if purpose == RequestPurpose.SIGNUP:
             if User.objects.filter(email=email).exists():
                 raise serializers.ValidationError({"email": "이미 가입된 이메일입니다."})
-            
+
         if purpose == RequestPurpose.FIND:
             if not User.objects.filter(email=email).exists():
                 raise serializers.ValidationError({"email": "가입되지 않는 이메일입니다."})
-            
+
         if purpose == RequestPurpose.RESTORE:
             try:
                 user = User.objects.prefetch_related("withdrawal_set").get(email=email)
@@ -46,6 +47,7 @@ class EmailRequestSerializer(Serializer[Any], BaseMixin):
 class SMSRequestSerializer(Serializer[Any], BaseMixin):
     phone_number = BaseMixin.get_phone_number_field()
     purpose = serializers.ChoiceField(choices=RequestPurpose.choices, default=RequestPurpose.SIGNUP)
+
     def validate(self, attrs: dict[str, Any]) -> dict[str, Any]:
         phone_number = attrs["phone_number"]
         purpose = attrs["purpose"]
@@ -53,14 +55,15 @@ class SMSRequestSerializer(Serializer[Any], BaseMixin):
         if purpose == RequestPurpose.SIGNUP:
             if User.objects.filter(phone_number=phone_number).exists():
                 raise serializers.ValidationError({"phone_number": "이미 가입된 전화번호입니다."})
-        
+
         if purpose == RequestPurpose.FIND:
             if not User.objects.filter(phone_number=phone_number).exists():
                 raise serializers.ValidationError({"phone_number": "가입되지 않는 전화번호입니다."})
-            
+
         if purpose == RequestPurpose.RESTORE:
             raise serializers.ValidationError("잘못된 요청입니다.")
 
+        return attrs
 
 
 class EmailCodeSerializer(Serializer[Any], BaseMixin):
