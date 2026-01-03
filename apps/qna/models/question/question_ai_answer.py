0a93fc1b88c3b74a6a471f278a895f32f350b400
hochang0819 <strongandrew@naver.com>
2026-01-03 21:53:11 +0900

    :bulb: chore(#306): model 수정

--- apps/qna/models/question/question_ai_answer.py
+++ apps/qna/models/question/question_ai_answer.py
@@ -1,26 +1,12 @@
-from typing import Optional
-
 from django.db import models
 
 from apps.core.models import TimeStampedModel
-from apps.qna.models.question.question_base import Question
-
+from apps.chatbot.models.chatbot_sessions import ChatModel
 
 class QuestionAIAnswer(TimeStampedModel):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="ai_answers")
-    question_id: Optional[int]
-
+    question = models.ForeignKey("qna.Question", on_delete=models.CASCADE)
     output = models.TextField()
-
-    USING_MODEL_CHOICES = [
-        ("GPT", "GPT"),
-        ("GEMINI", "GEMINI"),
-    ]
-
-    using_model = models.CharField(max_length=30, choices=USING_MODEL_CHOICES)
+    using_model = models.CharField(choices=ChatModel.choices, max_length=20)
 
     class Meta:
         db_table = "question_ai_answers"
-
-    def __str__(self) -> str:
-        return f"AI Answer for Question {self.question_id}"
