7a9b29294106d8b1c73fc27e08775f270ba0c103
hochang0819 <strongandrew@naver.com>
2026-01-04 00:04:47 +0900

    :bulb: chore(#306): apps 수정

--- apps/qna/apps.py
+++ apps/qna/apps.py
@@ -5,3 +5,12 @@ class QnaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
 
     name = "apps.qna"
+
+    """
+    앱 로딩 시 시그널/태스크 모듈 등록
+    Signal: import 되는 순간 등록
+    celery worker: task import 해야 task registry 등록됨
+    """
+    def ready(self) -> None:
+        import apps.qna.utils.ai_answer_signals
+        import apps.qna.utils.ai_answer_tasks
