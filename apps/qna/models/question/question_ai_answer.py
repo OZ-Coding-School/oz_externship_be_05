025c81f1f2421b6553cdd7fd94ce7b5f76fe8416
hochang0819 <strongandrew@naver.com>
2026-01-04 03:37:27 +0900

    :bulb: chore(#306): model 일부 수정

--- apps/qna/models/question/question_ai_answer.py
+++ apps/qna/models/question/question_ai_answer.py
@@ -4,7 +4,7 @@ from apps.core.models import TimeStampedModel
 from apps.chatbot.models.chatbot_sessions import ChatModel
 
 class QuestionAIAnswer(TimeStampedModel):
-    question = models.ForeignKey("qna.Question", on_delete=models.CASCADE)
+    question = models.ForeignKey("qna.Question", on_delete=models.CASCADE, related_name="ai_answers")
     output = models.TextField()
     using_model = models.CharField(choices=ChatModel.choices, max_length=20)
 
