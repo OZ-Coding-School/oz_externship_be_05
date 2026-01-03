77ed28c682666afb52d241cbb3e90badeee46b6f
hochang0819 <strongandrew@naver.com>
2026-01-04 03:45:18 +0900

    :sparkles: feature(#306): url 연결

--- apps/qna/urls/question_urls.py
+++ apps/qna/urls/question_urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from apps.qna.views.question.presigned_url_view import PresignedUploadAPIView
 from apps.qna.views.question.question_api import QuestionAPIView
 from apps.qna.views.question.question_detail import QuestionDetailAPIView
+from apps.qna.views.question.question_detail import QuestionAIAnswerAPIView
 
 urlpatterns = [
     path("questions", QuestionAPIView.as_view(), name="questions"),
     path("questions/<int:question_id>", QuestionDetailAPIView.as_view(), name="question_detail"),
+    path("questions/<int:question_id>/ai-answer", QuestionAIAnswerAPIView.as_view(), name="question_ai_answer"),
     # 이미지 업로드용
     path("questions/presigned-url/", PresignedUploadAPIView.as_view(), name="question_presigned_url"),
 ]
